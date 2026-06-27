771e8cf2a64d58899fa28e8054d8ffae1fea9ce4
Louai <louaibmmz@gmail.com>
2026-06-27 15:43:36 +0100

    feat(orchestration): Added the graph file that's responsible for the orchestration of the agent

--- agent/graph.py
+++ agent/graph.py
@@ -1,10 +1,17 @@
 from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.memory import MemorySaver
 from state import AgentState
+from nodes.enrichment_node import enrichment_node
+from checkpointer import get_checkpointer
 
 cash_agent = StateGraph(AgentState)
 
 # The orchestration of the agent
+cash_agent.add_node("enrichment", enrichment_node)
+
+cash_agent.set_entry_point("enrichment")
+
+cash_agent.add_edge("enrichment", END)
 
 checkpointer = MemorySaver()
-app = cash_agent.compile()
+app = cash_agent.compile(checkpointer=get_checkpointer(use_persistence=True))
