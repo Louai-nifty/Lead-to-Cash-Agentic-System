03c135d3af8c363af8a0276101194573b844488d
Louai <louaibmmz@gmail.com>
2026-06-27 15:13:45 +0100

    refactor: Remove old Database folder and cleanup

--- database/db.py
+++
@@ -1,8 +0,0 @@
-from supabase import create_client
-from config import SUPABASE_URL, SUPABASE_KEY
-
-client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-async def get_client():
-    """Return Supabase client (reuse same connection)."""
-    return client
