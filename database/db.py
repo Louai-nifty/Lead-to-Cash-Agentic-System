e7f89ea2a559b633d4447c88e7a49ddf7702700a
Louai <louaibmmz@gmail.com>
2026-06-27 15:11:00 +0100

    fix(databse): Fixed the db client to use the config instead of loading env and using the os

--- database/db.py
+++ database/db.py
@@ -1,12 +1,5 @@
 from supabase import create_client
-import os
-from dotenv import load_dotenv
-from postgrest.exceptions import APIError
-
-load_dotenv()
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+from config import SUPABASE_URL, SUPABASE_KEY
 
 client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
