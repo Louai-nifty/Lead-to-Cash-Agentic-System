5e1a5361396d08d8b5b2279dc742fb330dc10097
Louai <louaibmmz@gmail.com>
2026-06-29 23:56:59 +0100

    feat(graph): Updated the agent graph to contain the new node (scoring node), and added an edge to connected the existing nodes in the agent

--- agent/graph.py
+++ agent/graph.py
@@ -3,16 +3,20 @@ from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.memory import MemorySaver
 from .state import AgentState
 from .nodes.enrichment_node import enrichment_node
+from .nodes.scoring_node import scoring_node
 from .checkpointer import get_checkpointer
 
 graph = StateGraph(AgentState)
 
 # The orchestration of the agent
 graph.add_node("enrichment", enrichment_node)
+graph.add_node("scoring", scoring_node)
 
 graph.set_entry_point("enrichment")
 
-graph.add_edge("enrichment", END)
+graph.add_edge("enrichment", "scoring")
+
+graph.add_edge("scoring", END)
 
 checkpointer = MemorySaver()
 cash_agent = graph.compile(checkpointer=get_checkpointer(use_persistence=True))
