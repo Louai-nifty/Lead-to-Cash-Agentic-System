698a6dfd2615a13c66e2943a118200a826dbb2d7
Louai <louaibmmz@gmail.com>
2026-07-03 23:33:19 +0100

    feat(HITL): Implemented the 'Human-In-The-Loop' logic to the graph after the assignment route/node

--- agent/graph.py
+++ agent/graph.py
@@ -6,6 +6,7 @@ from .nodes.enrichment_node import enrichment_node
 from .nodes.scoring_node import scoring_node
 from .checkpointer import get_checkpointer
 from .nodes.assignment_node import assignment_node
+from .nodes.approval_handler_node import approval_handler_node
 
 graph = StateGraph(AgentState)
 
@@ -13,13 +14,15 @@ graph = StateGraph(AgentState)
 graph.add_node("enrichment", enrichment_node)
 graph.add_node("scoring", scoring_node)
 graph.add_node("assignment", assignment_node)
+graph.add_node("approval_handler", approval_handler_node)
 
 
 graph.set_entry_point("enrichment")
 
 graph.add_edge("enrichment", "scoring")
 graph.add_edge("scoring", "assignment")
-graph.add_edge("assignment", END)
+graph.add_edge("assignment", "approval_handler")
+graph.add_edge("approval_handler", END)
 
 checkpointer = MemorySaver()
-cash_agent = graph.compile(checkpointer=get_checkpointer(use_persistence=True))
+cash_agent = graph.compile(checkpointer=get_checkpointer(use_persistence=True), interrupt_before="approval_handler")
