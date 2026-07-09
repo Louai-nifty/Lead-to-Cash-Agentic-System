a454c4a67f865e227aabc29fd1d3fddf954b116e
Louai <louaibmmz@gmail.com>
2026-07-09 17:50:14 +0100

    fix: Added a missing state in the error exception

--- agent/nodes/assignment_node.py
+++ agent/nodes/assignment_node.py
@@ -26,7 +26,11 @@ async def assignment_node(state: AgentState):
         lead_source = lead_details["source"]
         lead_message = lead_details["message"]
         lead_company = lead_details["company"]
-            
+        
+        if routing_level == "Not_Qualified":
+            logger.info(f"Lead with email '{email}' is not qualified for assignment.")
+            state.status = "rejected"
+            return state
         
         if routing_level == "Junior_Rep":
             logger.info(f"Lead with email '{email}' has been assigned to a Junior Rep '{rep_name}'")
@@ -50,6 +54,7 @@ async def assignment_node(state: AgentState):
             state.rep_email = routing["assigned_to"]
             state.company = lead_company
             state.rep_name = rep_name
+            state.status = "assigned"
             
             return state
         
@@ -85,14 +90,17 @@ async def assignment_node(state: AgentState):
                 state.rep_email = routing["assigned_to"]
                 state.company = lead_company
                 state.rep_name = rep_name
+                state.status = "assigned"
                 
                 return state
             else:
                 logger.info(f"Lead deal size is greater than $10,000, sending to manager for approval")
+                manager_info = sup_client.table("Users").select("*").eq("role", "Manager").execute().data[0]
                 
+                manager_name = manager_info["name"]
                 lead_id = lead_details["id"]
                 
-                message = f"""New Lead Assigned
+                message = f"""New Lead Assigned to you manager {manager_name} for Approval
                                 Name: {lead_name} | Role: {lead_role}
                                 Company: Lead's company name: {lead_company}
                                 Email: {email}
@@ -130,9 +138,12 @@ async def assignment_node(state: AgentState):
                 await slack_notification_tool.ainvoke({"channel": approval_channel_id, "text": "Lead Requires Approval", "blocks": block_content})
                 
                 state.deal_size = deal_size
+                state.manager_name = manager_name
                 logger.info(f"Waiting for manager approval for lead with email '{email}'...")
+                state.status = "pending_approval"
                 return state
                     
     except Exception as e:
         logger.error(f"Assignment failed for {state.lead_email}: {str(e)}")
+        return state
 
