5665b5016f997afc428b1f8e90ea42d6cd51d7d6
Louai <louaibmmz@gmail.com>
2026-07-10 19:26:39 +0100

    fix: Fixed the request query sent to Supabase

--- services/routing_service.py
+++ services/routing_service.py
@@ -11,10 +11,12 @@ def routing_func(score, email):
             sup_client.table("Leads").update({"status": "rejected"}).eq("email", email).execute()
             return {"to_what_level": "Not_Qualified", "assigned_to": None, "assigned_rep_id": None, "assigned_rep_name": None}
         elif score >= 50 and score < 80:
-            Rep = sup_client.table("Users").select("id").eq("role", "Junior_Rep").eq("leads_assigned_atm", 0).execute().data[0]
+            Rep = sup_client.table("Users").select("id, name, email, leads_assigned_atm").eq("role", "Junior_Rep").eq("leads_assigned_atm", 0).execute().data[0]
+            
             Rep_id = Rep["id"]
             Rep_email = Rep["email"]
-            assigned_to = Rep["lead_assigned_atm"]
+            Rep_name = Rep["name"]
+            assigned_to = Rep["leads_assigned_atm"]
                 
             sup_client.table("Users").update({"leads_assigned_atm": assigned_to + 1}).eq("id", Rep_id).execute()
             sup_client.table("Leads").update({"status": "assigned", "assigned_rep_id": Rep_id}).eq("email", email).execute()
@@ -23,11 +25,12 @@ def routing_func(score, email):
             
             return {"to_what_level": "Junior_Rep", "assigned_to": Rep_email, "assigned_rep_id": Rep_id, "assigned_rep_name": Rep_name}
         else:
-            Rep = sup_client.table("Users").select("id").eq("role", "Senior_Rep").eq("leads_assigned_atm", 0).execute().data[0]
+            Rep = sup_client.table("Users").select("id, name, email, leads_assigned_atm").eq("role", "Senior_Rep").eq("leads_assigned_atm", 0).execute().data[0]
+            
             Rep_id = Rep["id"]
             Rep_email = Rep["email"]
             Rep_name = Rep["name"]
-            assigned_to = Rep["lead_assigned_atm"]
+            assigned_to = Rep["leads_assigned_atm"]
 
             sup_client.table("Users").update({"leads_assigned_atm": assigned_to + 1}).eq("id", Rep_id).execute()
             sup_client.table("Leads").update({"status": "assigned", "assigned_rep_id": Rep_id}).eq("email", email).execute()
