21d5c1e8da81a31a251421803c0ad7a019fba343
Louai <louaibmmz@gmail.com>
2026-08-05 23:43:51 +0100

    fix: Removed 'MemorySaver' and replaced it with a persistant memory (SQLite) for the HITL step to work

--- agent/graph.py
+++ agent/graph.py
@@ -1,6 +1,6 @@
 from langgraph import graph
 from langgraph.graph import StateGraph, END
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite.aio import AsyncSqliteSaver
 from .state import AgentState
 from .nodes.enrichment_node import enrichment_node
 from .nodes.scoring_node import scoring_node
@@ -11,7 +11,7 @@ from .nodes.proposal_node import proposal_node, proposal_send_node
 
 graph = StateGraph(AgentState)
 
-# The orchestration of the agent
+
 graph.add_node("enrichment", enrichment_node)
 graph.add_node("scoring", scoring_node)
 graph.add_node("assignment", assignment_node)
@@ -25,13 +25,18 @@ graph.set_entry_point("enrichment")
 graph.add_edge("enrichment", "scoring")
 graph.add_edge("scoring", "assignment")
 graph.add_edge("assignment", "approval_handler")
-graph.add_edge("proposal", "proposal_sender")
-graph.add_edge("proposal_sender", END)
 
 graph.add_conditional_edges(
     "approval_handler",
-    lambda state: END if state.status == "rejected" else "proposal"
+    lambda state: "proposal" if state.approval_decision == "approve" else END
 )
 
-checkpointer = MemorySaver()
-cash_agent = graph.compile(checkpointer=get_checkpointer(use_persistence=True), interrupt_before=["approval_handler"], interrupt_after=["assignment", "approval_handler", "proposal"])
+graph.add_edge("proposal", "proposal_sender")
+graph.add_edge("proposal_sender", END)
+
+checkpointer = AsyncSqliteSaver.from_conn_string("sqlite+aiosqlite:///checkpoints.db")
+cash_agent = graph.compile(
+    checkpointer=checkpointer, 
+    interrupt_before=["approval_handler"],
+    interrupt_after=["assignment", "proposal"]
+)
