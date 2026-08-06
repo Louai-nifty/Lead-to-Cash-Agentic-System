84fe58bb8e629f4d3717d6c8f572ac70de3abc2a
Louai <louaibmmz@gmail.com>
2026-08-06 18:53:58 +0100

    feat: Changed the format of the tool, so that it will support the difference between the usage of each one of the Slack Bots

--- agent/tools/notification.py
+++ agent/tools/notification.py
@@ -1,19 +1,27 @@
-from clients.slack_client import SlackClient
+from clients.slack_client import SlackProposalClient, SlackApprovalClient
 from langchain_core.tools import tool
 from utils.loggings import get_logger
 from typing import Optional
 
-client = SlackClient()
+approvalclient = SlackApprovalClient()
+proposalclient = SlackProposalClient()
 logger = get_logger(__name__)
 
 @tool
-async def slack_notification_tool(channel: str, text: str, blocks: Optional[list] = None):
+async def slack_notification_tool(channel: str, text: str, webhook_type: str, blocks: Optional[list] = None):
   """Send a Slack message to a channel with optional interactive blocks."""
   try:
       if blocks is None:
-        await client.send_message(channel, text) 
+        if webhook_type == "proposal":
+          await proposalclient.send_message(channel, text)
+        else:
+          await approvalclient.send_message(channel, text)
+
       else:
-        await client.send_approval_request(channel, text, blocks)
+        if webhook_type == "proposal":
+          await proposalclient.send_message(channel, text, blocks)
+        else:
+          await approvalclient.send_message(channel, text, blocks)
           
   except Exception as e:
       logger.error(f"Failed to send slack notification, {str(e)}")
