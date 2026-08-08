cee30f4668ed64bcac3f32e0d19681136016af0b
Louai <louaibmmz@gmail.com>
2026-08-09 00:02:57 +0100

    Delete: Removed an unused file

--- agent/checkpointer.py
+++
@@ -1,9 +0,0 @@
-from langgraph.checkpoint.memory import MemorySaver
-from dotenv import load_dotenv
-
-load_dotenv()  
-
-def get_checkpointer(use_persistence: bool = True):
-    if use_persistence:
-        return MemorySaver()
-    return None
